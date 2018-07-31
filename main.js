e9e69bb672980fb913694583d93ab5d90df0744a
Andreas Klein <mail@andreasklein.org>
2018-07-31 23:58:51 +0200

    Add newsletter subscription

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+var $ = require('jquery');
+
+//
+// Newsletter subscribe button field
+//
+
+$('[data-newsletter-subscription]').on('click', function (event) {
+    event.preventDefault();
+
+    var $button = $(this);
+    var $form = $button.closest('.newsletter');
+    var $email = $form.find('input[name="email"]');
+
+    if ($form.hasClass('is-active-newsletter-section')) {
+        $form.submit()
+    } else {
+        $form.addClass('is-active-newsletter-section');
+        $button.find('.newsletter__submit-text')
+                .text('Abbonieren')
+        $email.focus();
+    }
+});
 
